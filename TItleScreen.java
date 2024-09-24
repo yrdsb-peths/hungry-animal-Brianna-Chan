e1c017b4eeab52386ce3cb23ac957c2abe1a83e0
Brianna-Chan <348835448@gapps.yrdsb.ca>
2024-09-24 10:23:12 -0400

    add more title screen + add levels

--- TItleScreen.java
+++ TItleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TItleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
+    Label titleLabel = new Label("The Elephant", 60);
     /**
      * Constructor for objects of class TItleScreen.
      * 
@@ -17,10 +17,11 @@ public class TItleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, 251, 130);
+        prepare();
     }
-    
+
     /**
      * The main world act loop
      */
@@ -32,5 +33,21 @@ public class TItleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,468,134);
+        elephant.setLocation(459,117);
+        Label label = new Label("Press <space > to start", 40);
+        addObject(label,290,246);
+        Label label2 = new Label("Used \u2190 and \u2192 to Move", 40);
+        addObject(label2,294,197);
+
     }
 }
